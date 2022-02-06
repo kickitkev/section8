b6599f72cf1f370eb36dfc4c163355a70efe6164
kickitkev <kevin@creatvek.co.uk>
2022-02-06 20:16:52 +0000

    add user form component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import logo from "./logo.svg";
+import React, { useEffect } from "react";
+import BasicCard from "./components/UI/Card";
+import UserForm from "./components/features/UserForm";
 import "./App.css";
 
 function App() {
+  const [userDetails, setUserDetails] = React.useState([{}]);
+
+  const addUserHandler = (enteredName, enteredAge) => {
+    setUserDetails([
+      ...userDetails,
+      {
+        name: enteredName,
+        age: enteredAge,
+        id: Math.random(),
+      },
+    ]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <>
+      <BasicCard variant="outlined">
+        <UserForm onAddUser={addUserHandler} />
+        <ul>
+          {userDetails.map((user) => (
+            <li key={user.id}>
+              <p>{user.name}</p>
+              <p>{user.age}</p>
+            </li>
+          ))}
+        </ul>
+      </BasicCard>
+    </>
   );
 }
 
